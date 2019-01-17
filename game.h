3b58038badeed29874f08eeff560c42b2fb35a19
MinorLegato <anton.siderud@gmail.com>
2019-01-17 20:05:37 +0100

    camera follows player!

--- game.h
+++ game.h
@@ -15,22 +15,15 @@ static void game_init(GameState* gs) {
     memset(gs, 0, sizeof *gs);
 
     tilemap_generate_random(&gs->map);
+
     player_create(&gs->player, v2(5, 5));
 }
 
 static void game_update(GameState* gs, float t) {
-    camera_update(&gs->camera, v2(10.0f, 10.0f), t);
+    camera_update(&gs->camera, gs->player.box[0].pos, t);
 }
 
 static void game_render(GameState* gs) {
-    ce_set_view(
-        8.0f, 8.0f, 16.0f,
-        8.0f, 8.0f, 0.0f,
-        0.0f,  1.0f, 0.0f,
-        60.0f, 0.1f, 64.0f);
-
-    ce_set_light(0.0f, 0.0f, 16.0f, 1.0f, 1.0f, 1.0f);
-
     tilemap_render(&gs->map);
 
     entity_render(gs->player.box, gs->player.count);
