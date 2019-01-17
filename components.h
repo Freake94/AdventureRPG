67d37104a97b4a59504eaa6531022aa95bff83b8
Freake94 <marblc15@student.hh.se>
2019-01-17 17:37:24 +0100

    comit1

--- components.h
+++ components.h
@@ -5,11 +5,48 @@
 
 // TODO: entity components:
 
+/* BOX */
+
 typedef struct Box {
     v2      pos;
     v2      vel;
     v2      rad;
 } Box;
+
+static Box box_create(const v2 pos, const v2 vel, const v2 rad) {
+    Box box;
+    copy2v(box.pos, pos);
+    copy2v(box.vel, vel);
+    copy2v(box.rad, rad);
+    return box;
+}
+
+/* Health */
+
+typedef struct Health {
+    float val;
+    float max;
+    int dead;
+} Health;
+
+static Health health_create(float val) {
+    Health health;
+    health.val = val;
+    health.max = val;
+    return health;
+}
+
+static void mark_dead(Health *health, int count) {
+    for(int i = 0; i < count; ++i) {
+        if(health[i].val <= 0) {
+            health[i].dead = 1;
+        }
+    }
+}
+
+// TODO(Blüch): Equipment System
+
+// TODO(Blüch): Inventory System
 
 #endif
 
--- entity.h
+++ entity.h
@@ -3,7 +3,18 @@
 #ifndef __ENTITY_H__
 #define __ENTITY_H__
 
-// TODO: add some fucking entity shit
+#define PLAYER_MAX 4
+#define ENEMY_MAX 16
+
+typedef struct Player {
+    int count;
+    Box box[PLAYER_MAX];
+} Player;
+
+typedef struct Slime {
+    int count;
+    Box box[ENEMY_MAX];
+    int dead;
+} Slime;
 
 #endif
-
